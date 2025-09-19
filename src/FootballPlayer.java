6fd4861069bcc874ac06ff452c24f140de2ade40
Gina-Asang <ginagana05@gmail.com>
2025-09-19 10:59:19 +0000

    Creating a new project and Java class

---
+++ src/FootballPlayer.java
@@ -0,0 +1,27 @@
+/* Author: Gina Gana
+   Date: 19/09/25
+   Lab_01_cohortA
+   Objectives
+• Practice basic Java syntax with variables, data types, and initialization.
+• Apply type casting, increment/decrement operators, and logical operators.
+• Use selection statements: if/else, nested ifs, switch, and conditional expressions.
+• Understand and practice switch fall-through (running cases), both as a mistake and as a
+useful grouping technique.
+• Combine all concepts into a single, working player selection simulator
+
+Final Results
+At the end of the lab, you will have a Java program that:
+• Accepts player details (name, age, height, weight, jersey number) from the keyboard.
+• Converts height to centimeters and rounds weight.
+• Categorizes the player as Rising Star, Prime Player, or Veteran.
+• Identifies the player’s position based on the jersey number.
+• Determines whether the jersey number is an attacker number.
+• Evaluates eligibility using logical conditions.
+• Decides whether the player is in the starting lineup (nested if).
+• Outputs a structured “Player Report” with a final decision: Play or Rest.
+• Demonstrates how a switch behaves with missing breaks (unwanted fall-through) and
+how to use grouped cases correctly
+ */
+public class FootballPlayer {
+
+}
