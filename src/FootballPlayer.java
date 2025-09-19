47e540c4720dda60159b4db7fb22341bf0152485
Gina Asang Gana <ginagana05@gmail.com>
2025-09-19 11:07:04 +0000

    Update FootballPlayer.java

--- src/FootballPlayer.java
+++ src/FootballPlayer.java
@@ -8,19 +8,6 @@
 • Understand and practice switch fall-through (running cases), both as a mistake and as a
 useful grouping technique.
 • Combine all concepts into a single, working player selection simulator
-
-Final Results
-At the end of the lab, you will have a Java program that:
-• Accepts player details (name, age, height, weight, jersey number) from the keyboard.
-• Converts height to centimeters and rounds weight.
-• Categorizes the player as Rising Star, Prime Player, or Veteran.
-• Identifies the player’s position based on the jersey number.
-• Determines whether the jersey number is an attacker number.
-• Evaluates eligibility using logical conditions.
-• Decides whether the player is in the starting lineup (nested if).
-• Outputs a structured “Player Report” with a final decision: Play or Rest.
-• Demonstrates how a switch behaves with missing breaks (unwanted fall-through) and
-how to use grouped cases correctly
  */
 public class FootballPlayer {
 
